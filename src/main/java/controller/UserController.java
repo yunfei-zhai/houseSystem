254906787328afb9ee66277d324c05c12376426d
Administrator <Administrator@WIN-445BQ674SR6>
2020-01-02 10:39:47 +0800

    验证码更新改良

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,7 +1,10 @@
 package controller;
 
-import java.util.List;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import model.User;
 import service.UserService;
+import utils.ImageVerificationCode;
 import utils.ReturnInfo;
 import utils.ReturnJson;
 
@@ -75,5 +79,18 @@ public class UserController {
 		service.delete(id);
 		return new ReturnJson();
 	}
-	
+	//获取验证码图片
+	@RequestMapping("getVerifiCode")
+    @ResponseBody
+    public void getVerifiCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        /*
+             1.生成验证码
+             2.把验证码上的文本存在session中
+             3.把验证码图片发送给客户端
+             */
+        ImageVerificationCode ivc = new ImageVerificationCode();     //用我们的验证码类，生成验证码类对象
+        BufferedImage image = ivc.getImage();  //获取验证码
+        request.getSession().setAttribute("number", ivc.getText()); //将验证码的文本存在session中
+        ivc.output(image, response.getOutputStream());//将验证码图片响应给客户端
+    }
 }
